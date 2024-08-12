1f23028191bd1c0cce9d72ad23e20ff0fe7468c5
zizan-azman <azizan.azman.fe@gmail.com>
2024-08-12 12:27:56 +0800

    added shared ui components

---
+++ libs/maine/shared/ui/src/lib/navbar/navbar-default/navbar-default.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-navbar-default',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar-default.component.html',
+  styleUrl: './navbar-default.component.scss',
+})
+export class NavbarDefaultComponent {}
