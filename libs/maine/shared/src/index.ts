a26f8becab7fb581beacd2024a3209b0c9715812
zizan-azman <azizan.azman.fe@gmail.com>
2024-08-13 22:20:59 +0800

    created button-default component

--- libs/maine/shared/src/index.ts
+++ libs/maine/shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/ui/navbar/navbar-default/navbar-default.component';
 export * from './lib/ui/navbar/navbar-default/navbar-default.model';
+
+export * from './lib/ui/button/button-default/button-default.component';
