2de6e0b78eafe7ccd56e38ad167eac8f5af3333f
zizan-azman <azizan.azman.fe@gmail.com>
2024-10-06 14:22:48 +0800

    added icon to new layout

--- libs/maine/shared/src/lib/ui/layout/layout-background-content/layout-background-content.component.ts
+++ libs/maine/shared/src/lib/ui/layout/layout-background-content/layout-background-content.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutBackgroundContentConfig } from './layout-background-content.model';
 
 @Component({
   selector: 'maine-layout-background-content',
@@ -8,4 +9,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './layout-background-content.component.html',
   styleUrl: './layout-background-content.component.scss',
 })
-export class LayoutBackgroundContentComponent {}
+export class LayoutBackgroundContentComponent {
+  layoutBackgroundContentBgColorPlaceholder: LayoutBackgroundContentConfig['bgColor'] =
+    'default';
+  layoutBackgroundContentSpacingPlaceholder: LayoutBackgroundContentConfig['spacing'] =
+    'small';
+
+  @Input() bgColor: LayoutBackgroundContentConfig['bgColor'] =
+    this.layoutBackgroundContentBgColorPlaceholder;
+
+  @Input() spacing: LayoutBackgroundContentConfig['spacing'] =
+    this.layoutBackgroundContentSpacingPlaceholder;
+
+  getSpacing() {
+    if (this.spacing === 'small') {
+      return 'spacing-sm';
+    } else {
+      return 'spacing-md';
+    }
+  }
+}
